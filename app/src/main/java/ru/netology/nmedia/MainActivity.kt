38c1eb3c05af2d56f5c143aa358db5c5f67ef20c
Alex Sergeev <sanhokk@yandex.ru>
2024-01-22 16:46:08 +0300

    used mvvm architecture

--- app/src/main/java/ru/netology/nmedia/MainActivity.kt
+++ app/src/main/java/ru/netology/nmedia/MainActivity.kt
@@ -4,24 +4,16 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ImageButton
 import android.widget.ImageView
+import androidx.activity.viewModels
 import ru.netology.nmedia.databinding.ActivityMainBinding
 import ru.netology.nmedia.dto.Post
+import ru.netology.nmedia.viewmodel.PostViewModel
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-        val post = Post(
-            id = 1,
-            author = "Нетология. Университет интернет-профессий будущего",
-            content = "Привет, это новая Нетология! Когда-то Нетология начиналась с интенсивов по онлайн-маркетингу. Затем появились курсы по дизайну, разработке, аналитике и управлению. Мы растём сами и помогаем расти студентам: от новичков до уверенных профессионалов. Но самое важное остаётся с нами: мы верим, что в каждом уже есть сила, которая заставляет хотеть больше, целиться выше, бежать быстрее. Наша миссия — помочь встать на путь роста и начать цепочку перемен → http://netolo.gy/fyb",
-            published = "21 мая в 18:36",
-            likedByMe = false,
-            likes = 0,
-            shares = 0
-        )
 
         fun Int.toShortString(): String = when (this) {
             in 0..<1_000 -> this.toString()
@@ -32,26 +24,27 @@ class MainActivity : AppCompatActivity() {
             else -> "MANY"
         }
 
-        with(binding) {
-            author.text = post.author
-            data.text = post.published
-            mainText.text = post.content
-        if(post.likedByMe) {
-            likes.setImageResource(R.drawable.liked)
-        }
-
-            likes.setOnClickListener {
-                post.likedByMe = !post.likedByMe
-
+        val viewModel: PostViewModel by viewModels()
+        viewModel.data.observe(this) { post ->
+            with(binding) {
+                author.text = post.author
+                data.text = post.published
+                mainText.text = post.content
                 likes.setImageResource(
                     if (post.likedByMe) R.drawable.liked else R.drawable.likes
                 )
-                if (post.likedByMe) binding.likesCounter.text = (++post.likes).toShortString()
-                else binding.likesCounter.text = (--post.likes).toShortString()
-
             }
-            reposts.setOnClickListener {
-                binding.repostCounter.text = (++post.shares).toShortString()
+
+            binding.likes.setOnClickListener {
+                viewModel.like()
+                binding.likesCounter.text = post.likes.toShortString()
+
+//                if (post.likedByMe) binding.likesCounter.text = (++post.likes).toShortString()
+//                else binding.likesCounter.text = (--post.likes).toShortString()
+            }
+            binding.reposts.setOnClickListener {
+                viewModel.share()
+                binding.repostCounter.text = post.shares.toShortString()
             }
         }
     }
