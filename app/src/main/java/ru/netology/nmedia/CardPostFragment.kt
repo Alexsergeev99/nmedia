bbbd3f6f8bd197c733856ddd49051e97db5a7ae6
Alex Sergeev <sanhokk@yandex.ru>
2024-05-17 21:38:16 +0300

    fix error

--- app/src/main/java/ru/netology/nmedia/CardPostFragment.kt
+++ app/src/main/java/ru/netology/nmedia/CardPostFragment.kt
@@ -2,21 +2,16 @@ package ru.netology.nmedia
 
 import android.app.AlertDialog
 import android.content.Intent
-import android.net.Uri
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.PopupMenu
 import androidx.activity.OnBackPressedCallback
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
-import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
-import ru.netology.nmedia.EditPostFragment.Companion.idArg
 import ru.netology.nmedia.EditPostFragment.Companion.textArg
 import ru.netology.nmedia.databinding.FragmentCardPostBinding
-import ru.netology.nmedia.databinding.FragmentNewPostBinding
 import ru.netology.nmedia.dto.Post
 import ru.netology.nmedia.util.IntArg
 import ru.netology.nmedia.util.StringArg
@@ -73,6 +68,14 @@ class CardPostFragment : Fragment() {
 
                     override fun onClick(post: Post) {
 //                        findNavController().navigate(R.id.action_feedFragment_to_cardPostFragment)
+                    }
+
+                    override fun onClickPhoto(post: Post) {
+                        findNavController().navigate(R.id.action_cardPostFragment_to_photoFragment,
+                            Bundle().apply {
+                                textArg = post.attachment?.url
+                            }
+                        )
                     }
 
                     override fun onEdit(post: Post) {
